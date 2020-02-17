3dff87ead8530465c5b95b8a5fb16116d73bbdbf
Marco Zingoni <marco.zingoni@gmail.com>
2020-02-17 20:08:37 +0100

    Cors

--- app.py
+++ app.py
@@ -3,6 +3,7 @@ import os
 from flask import Flask
 from flask_restful import Api
 from flask_jwt import JWT
+from flask_cors import CORS, cross_origin
 
 from security import authenticate, identity
 from resources.user import UserRegister
@@ -10,10 +11,13 @@ from resources.item import Item, ItemList
 from resources.store import Store, StoreList
 
 app = Flask(__name__)
+cors = CORS(app)
 
 app.config['DEBUG'] = True
+app.config['CORS_HEADERS'] = 'Content-Type'
 
-app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DATABASE_URL', 'sqlite:///data.db')
+app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get(
+    'DATABASE_URL', 'sqlite:///data.db')
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.secret_key = 'jose'
 api = Api(app)
